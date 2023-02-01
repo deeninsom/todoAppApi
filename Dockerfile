ac58d272c348989fa0d1066874557b59dc710a7f
deeninsom <syihab855@gmail.com>
2023-02-02 02:57:05 +0700

    updated

--- Dockerfile
+++ Dockerfile
@@ -5,10 +5,12 @@ WORKDIR /usr/src/app
 COPY package.json ./
 
 RUN npm install
+# RUN npx sequelize init
+# RUN npx sequelize db:migrate
 
 COPY . .
 
 EXPOSE 3030
 
-CMD [ "npm", "run", "dev" ]
+CMD [ "npm", "start" ]
 
--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express')
-// import connectDb from './config/db.js'
+
 const activityRoute = require("./component/activities/activityRoute");
 const todoRoute = require('./component/todos/todoRoute');
-// import todoRoute from "./component/todo/routes/todoRoute.js";
+
 require('dotenv').config()
 const app = express();
 
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3030
 
 app.use(express.json())
 app.use('/todolist.api.devcode.gethired.id', activityRoute, todoRoute)
-// app.use('/todolist.api.devcode.gethired.id')
+
 
 app.listen(PORT, ()=>{
     // connectDb
